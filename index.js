3d1b8c6d1ef53bdb48fd8b40f39b033db8f6976b
Lewis Moten <lewis.moten@codejamboree.com>
2024-04-12 12:51:05 -0400

    add 88 key piano

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let waveFormRadios;
 let frequencyInput;
 let oscilloscopeCanvas;
 let customWaveFormData = [];
+let pianoCanvas;
 
 function randomNum(min, max) {
   return Math.random() * (max - min) + min;
@@ -48,17 +49,29 @@ function getDurationSeconds() {
 function getFrequency() {
   return parseInt(document.getElementById('frequency').value);
 }
+function setFrequency(hertz) {
+  document.getElementById('frequency').value = hertz;
+}
 
 function getWaveForm() {
   return document.querySelector('input[name="waveType"]:checked').value;
 }
+let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 function handleClick() {
+  if(audioContext.state === 'suspended') {
+    audioContext.resume();
+  } else if(audioContext.state === 'closed') {
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  } else if (audioContext.state === 'running') {
+    // console.log('Audio context is running');
+  } else if (audioContext.state === 'interrupted') {
+    console.log('Audio context is interrupted');
+  }
   const durationSeconds = getDurationSeconds();
   const frequency = getFrequency();
   const waveType = getWaveForm();
   const channelCount = 1;
-  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
   let sound = getSound(waveType, audioContext, channelCount, frequency, durationSeconds);
 
   sound.connect(audioContext.destination);
@@ -184,6 +197,144 @@ function handleCustomWaveFormClick(event) {
     handleWaveFormChange();
 }
 
+
+const piano = {
+  keyNumber: 0,
+  frequency: 0,
+  pressed: false,
+  black: false,
+  lastKey: 0,
+  x: 0,
+  y: 0
+}
+const pianoLetters = 'ABCDEFG';
+const musicSharp = '\u266F';
+const musicFlat = '\u266D';
+let pianoPressed = false;
+
+function handlePianoMouseDown(){
+  pianoPressed = true;
+}
+function handlePianoMouseUp() {
+  pianoPressed = false;
+}
+
+function handlePianoMouseMove(event) {
+  const {
+    left,
+    top,
+    height,
+    width
+  } = pianoCanvas.getBoundingClientRect();
+  const {
+    clientX,
+    clientY
+  } = event;
+  const mouseX = clientX - left;
+  const mouseY = clientY - top;
+
+  const KEY_D = 4;
+  const KEY_A = 1;
+  const whiteKeyCount = 52;
+  const keyHeight = pianoCanvas.height;
+  const keyWidth = pianoCanvas.width / whiteKeyCount;
+  const blackKeyCount = whiteKeyCount - 1;
+  const blackKeyWidth = keyWidth * 0.8;
+  const blackKeyHeight = keyHeight * 0.6;
+  const blackKeyOffset = keyWidth * 0.6;
+
+  piano.x = mouseX;
+  piano.y = mouseY;
+  piano.keyNumber = (Math.floor(mouseX / keyWidth) * 2) + 1;
+  piano.letter = pianoLetters[piano.keyNumber % 7];
+  piano.octave = Math.floor(piano.keyNumber / 7) - 1;
+  piano.sharpFlat = false;
+
+
+  let keyNumber = 0;
+  let found = false;
+  for(let i = 0; i < whiteKeyCount; i++) {
+    keyNumber++;
+    const x = i * keyWidth;
+    if(mouseX >= x && mouseX <= x + keyWidth && mouseY >= 0 && mouseY <= keyHeight) {
+      piano.keyNumber = keyNumber;
+      piano.sharpFlat = false;
+      piano.letter = pianoLetters[i % 7];
+      found = true;
+    }
+
+    if(i % 7 !== KEY_D && i % 7 !== KEY_A) {
+      keyNumber++;
+      const x = i * keyWidth + blackKeyOffset;
+      if(mouseX >= x && mouseX <= x + blackKeyWidth && mouseY >= 0 && mouseY <= blackKeyHeight) {
+        piano.keyNumber = keyNumber;
+        piano.sharpFlat = true;
+        piano.letter = pianoLetters[i % 7] + musicSharp + ' ' + pianoLetters[(i+1) % 7] + musicFlat;
+        found = true;
+      }
+    }
+
+    if(found) break;
+  }
+
+  piano.frequency = 440 * Math.pow(2, (piano.keyNumber - 50) / 12);
+  setFrequency(piano.frequency);
+  drawPiano();
+  if(pianoPressed && piano.keyNumber !== piano.lastKey) {
+    piano.lastKey = piano.keyNumber;
+    handleClick();
+  }
+}
+
+function drawPiano() {
+  const KEY_D = 4;
+  const KEY_A = 1;
+  const whiteKeyCount = 52;
+  const keyHeight = pianoCanvas.height;
+  const keyWidth = pianoCanvas.width / whiteKeyCount;
+  const blackKeyCount = whiteKeyCount - 1;
+
+  const ctx = pianoCanvas.getContext('2d');
+  ctx.fillStyle = 'white';
+  
+  let keyNumber = 0;
+  for (let i = 0; i < whiteKeyCount; i++) {
+    keyNumber++;
+      const x = i * keyWidth;
+      if(piano.keyNumber === keyNumber) {
+        ctx.fillStyle = '#cccccc';
+      } else {
+        ctx.fillStyle = 'white';
+      }
+      ctx.fillRect(x, 0, keyWidth, keyHeight);
+      ctx.strokeRect(x, 0, keyWidth, keyHeight);
+      if (i % 7 !== KEY_D && i % 7 !== KEY_A) { keyNumber++;}
+  }
+
+  const blackKeyWidth = keyWidth * 0.8;
+  const blackKeyHeight = keyHeight * 0.6;
+  const blackKeyOffset = keyWidth * 0.6;
+  ctx.fillStyle = 'black';
+  keyNumber = 0;
+  for (let i = 0; i < blackKeyCount; i++) {
+    keyNumber++;
+      if (i % 7 !== KEY_D && i % 7 !== KEY_A) {
+        keyNumber++;
+        if(piano.keyNumber === keyNumber) {
+          ctx.fillStyle = '#777777';
+        } else {
+          ctx.fillStyle = 'black';
+        }
+          const x = i * keyWidth + blackKeyOffset;
+          ctx.fillRect(x, 0, blackKeyWidth, blackKeyHeight);
+          ctx.strokeRect(x, 0, blackKeyWidth, blackKeyHeight);
+      }
+  }
+  ctx.fillStyle = 'red';
+  ctx.font = '20px Arial';
+  ctx.fillText(`Key Number: ${piano.keyNumber} : ${piano.letter}`, 0, 20);
+}
+
 function handleCustomWaveFormMouseMove(event) {
   if(!customWaveformDrawing) return;
   handleCustomWaveFormClick(event);
@@ -191,6 +342,8 @@ function handleCustomWaveFormMouseMove(event) {
 function handleLoad() {
   btn = document.getElementById('btn');
   btn.addEventListener('click', handleClick);
+  pianoCanvas = document.getElementById('piano');
+  drawPiano();
   customWaveformCanvas = document.getElementById('customWaveform');
   customWaveFormData = Array(20).fill(.5);
   oscilloscopeCanvas = document.getElementById('oscilloscope');
@@ -204,7 +357,12 @@ function handleLoad() {
   customWaveformCanvas.addEventListener('mousedown', handleCustomWaveFormMouseDown)
   customWaveformCanvas.addEventListener('mouseup', handleCustomWaveFormMouseUp)
   customWaveformCanvas.addEventListener('mouseout', handleCustomWaveFormMouseUp)
-  customWaveformCanvas.addEventListener('click', handleCustomWaveFormClick)
+  customWaveformCanvas.addEventListener('click', handleCustomWaveFormClick);
+  pianoCanvas.addEventListener('mousemove', handlePianoMouseMove);
+  pianoCanvas.addEventListener('mousedown', handlePianoMouseDown);
+  pianoCanvas.addEventListener('mouseup', handlePianoMouseUp);
+  pianoCanvas.addEventListener('mouseout', handlePianoMouseUp);
+  pianoCanvas.addEventListener('click', handleClick);
 }
 
 window.addEventListener('load',handleLoad);
